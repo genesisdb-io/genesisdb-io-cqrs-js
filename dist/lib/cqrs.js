c6ad25fc49afd354719c433ed647b2e6ada62528
patriceckhart <mail@patriceckhart.com>
2025-06-19 14:03:18 +0200

    0.0.3

--- dist/lib/cqrs.js
+++ dist/lib/cqrs.js
@@ -1,11 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.registerCommand = registerCommand;
+exports.dispatchCommand = dispatchCommand;
 const registry = new Map();
-export function registerCommand(type, handler) {
+function registerCommand(type, handler) {
     if (registry.has(type)) {
         throw new Error(`Command '${type}' is already registered`);
     }
     registry.set(type, handler);
 }
-export async function dispatchCommand(command) {
+async function dispatchCommand(command) {
     const handler = registry.get(command.type);
     if (!handler) {
         throw new Error(`No handler for command '${command.type}'`);
